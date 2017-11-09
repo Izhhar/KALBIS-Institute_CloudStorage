0dc068ee1d7558d9cfa8df52043b0daba16c6bfc
Izhhar <izardos@gmail.com>
2017-11-09 08:15:46 +0700

    Add files via upload

---
+++ files/index.php
@@ -0,0 +1,15 @@
+<?php require_once "include/getdata.php" ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=module/index.php">
+<title>Drive</title>
+<script language="javascript">
+    window.location.href = "module/index.php"
+</script>
+</head>
+<body>
+Go to <a href="module/index.php">/module/index.php</a>
+</body>
+</html>
